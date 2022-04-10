9820df2889fa737a7506cac4f9f76a0e0368bff2
Валерий Чесноков <valery.chesnokov@gmail.com>
2022-04-10 22:52:03 +0300

    Add project files.

---
+++ YoutubeMe/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YoutubeMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private YoutubeGetMe youtubeGetMe = null;
+
+        const string LOG_FILE = "log.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (FileStream fs = File.Create(LOG_FILE)) { }
+
+            lbLogInfo.Content = $"Смотрите ЖУРНАЛ действий в файле: {LOG_FILE}";
+
+            youtubeGetMe = new YoutubeGetMe();
+            youtubeGetMe.PushEvent += YoutubeGetMe_PushEvent;
+            youtubeGetMe.PushVideoCountEvent += YoutubeGetMe_PushVideoCountEvent;
+
+        }
+
+        private void YoutubeGetMe_PushVideoCountEvent(object sender, EventArgs e)
+        {
+            YoutubeMeDownloadEventArgs ea = (YoutubeMeDownloadEventArgs)e;
+
+            lbVideoCount.Content = $"Найдено видео (шт.): {ea.Message}";
+        }
+
+        private void YoutubeGetMe_PushEvent(object sender, EventArgs e)
+        {
+            YoutubeMeDownloadEventArgs ea = (YoutubeMeDownloadEventArgs)e;
+
+            addToLog(ea.Message);
+        }
+
+        private void addToLog(string message)
+        {
+            //txtLog.Text += "\r\n" + message;
+            txtLog.Items.Add("\r\n" + message);
+
+            AppendTextToLog(message);
+        }
+
+        private void AppendTextToLog(string message)
+        {
+            using (StreamWriter w = File.AppendText(LOG_FILE))
+            {
+                w.WriteLine(message);
+            }
+        }
+
+        private void btnGetList_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = txtYoutubeAddress.Text;
+                //int intOrderVideoNumber = 1;
+                //intOrderVideoNumber = int.Parse(txtOrderVideoNumber.Text);
+
+                youtubeGetMe.GetList(url); ///, intOrderVideoNumber);
+
+                btnDownload.IsEnabled = true;
+                //youtubeGetMe.DownloadList();
+
+                //MessageBox.Show("btnGetList_Click");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int intOrderVideoNumber = 1;
+                intOrderVideoNumber = int.Parse(txtOrderVideoNumber.Text);
+
+                youtubeGetMe.DownloadList(intOrderVideoNumber);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
---
+++ YoutubeMe/YoutubeGetMe.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeMe
+{
+    public class YoutubeGetMe
+    {
+        private System.Collections.Generic.List<YoutubeExplode.Playlists.PlaylistVideo> m_videos = null;
+        private YoutubeExplode.YoutubeClient m_youtubeClient = new YoutubeClient();
+
+        public event EventHandler PushEvent;
+        public event EventHandler PushVideoCountEvent;
+
+        protected virtual void OnPushEvent(EventArgs e)
+        {
+            EventHandler handler = PushEvent;
+            handler?.Invoke(this, e);
+        }
+
+        protected virtual void OnPushVideoCountEvent(EventArgs e)
+        {
+            EventHandler handler = PushVideoCountEvent;
+            handler?.Invoke(this, e);
+        }
+
+        public async void GetList(string channel_id) //, int intOrderVideoNumber)
+        {
+            if (m_youtubeClient == null)
+                m_youtubeClient = new YoutubeClient();
+
+            m_videos = (System.Collections.Generic.List<YoutubeExplode.Playlists.PlaylistVideo>)await m_youtubeClient.Channels.GetUploadsAsync(channel_id);
+
+            OnPushVideoCountEvent(new YoutubeMeDownloadEventArgs { Message = $"{m_videos.Count}" });
+
+            ///DownloadList(intOrderVideoNumber);
+
+            OnPushEvent(new YoutubeMeDownloadEventArgs { Message = $"Найдены Ютуб ролики:" });
+            int index = 0;
+
+            foreach (YoutubeExplode.Playlists.PlaylistVideo video_item in m_videos)
+            {
+                ++index;
+                OnPushEvent(new YoutubeMeDownloadEventArgs { Message = $"{index}): {video_item.Url} - {video_item.Title}" });
+            } // foreach (YoutubeExplode.Playlists.PlaylistVideo video_item in m_videos)
+
+        }
+
+        public async void DownloadList(int intOrderVideoNumber)
+        {
+            if (m_videos == null)
+                return;
+
+            string localFileName;
+            int index = 0;
+
+            foreach (YoutubeExplode.Playlists.PlaylistVideo video_item in m_videos)
+            {
+                ++index;
+
+                if (index < intOrderVideoNumber)
+                {
+                    OnPushEvent(new YoutubeMeDownloadEventArgs { Message = $"{index}) Пропускаем файл: {video_item.Url} - {video_item.Title}" });
+                    continue;
+                }
+
+                localFileName = $"{video_item.Title}.mp4"; // video_item.Title video_item.Id
+                
+                OnPushEvent(new YoutubeMeDownloadEventArgs { Message = $"{index}) Скачивание файла: {video_item.Url} - {video_item.Title}" });
+
+                var streamManifest = await m_youtubeClient.Videos.Streams.GetManifestAsync(video_item.Id);
+                //var streamInfo = streamManifest.GetVideoOnlyStreams().First();
+                var streamInfo = streamManifest.GetMuxedStreams().TryGetWithHighestVideoQuality();
+
+                if (streamInfo != null)
+                {
+                    await m_youtubeClient.Videos.Streams.DownloadAsync(streamInfo, localFileName);
+                }
+                else
+                {
+                    OnPushEvent(new YoutubeMeDownloadEventArgs { Message = $"Ошибка скачивания файла: {video_item.Url}" });
+                }
+            } // foreach (YoutubeExplode.Playlists.PlaylistVideo video_item in m_videos)
+        }
+    }
+}
